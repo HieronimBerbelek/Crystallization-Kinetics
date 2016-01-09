049d6812a828a0f490a3d6a9815933ecd4096a5c
Hieronim Berbelek <gutek_137@wp.pl>
2016-01-09 12:05:11 +0100

    Signed-off-by: Hieronim Berbelek <gutek_137@wp.pl>

--- src/test/java/dataDSCTest/DataTupleBuilderTest.java
+++ src/test/java/dataDSCTest/DataTupleBuilderTest.java
@@ -3,58 +3,8 @@ package dataDSCTest;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+//not testing basic setters and getters
 public class DataTupleBuilderTest {
-
-	@Test
-	public void testSetTemperature() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetTime() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetDsc() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetSensitivity() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetSegment() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetTemperature() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetTime() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetDsc() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetSensitivity() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetSegment() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	public void testGetData() {

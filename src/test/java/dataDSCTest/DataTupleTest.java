f5d09766a48b6db1114a3dce96ed5a2d0ec18965
Hieronim Berbelek <gutek_137@wp.pl>
2016-01-09 12:05:11 +0100

    Signed-off-by: Hieronim Berbelek <gutek_137@wp.pl>

--- src/test/java/dataDSCTest/DataTupleTest.java
+++ src/test/java/dataDSCTest/DataTupleTest.java
@@ -9,31 +9,5 @@ public class DataTupleTest {
 	@Test
 	public void testDataTuple() {
 		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetTemperature() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetTime() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetDsc() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetSensitivity() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetSegment() {
-		fail("Not yet implemented");
-	}
-
+	}	
 }

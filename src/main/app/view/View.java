a2fe648fced53750795cf4a464fb81d36d760419
Hieronim Berbelek <gutek_137@wp.pl>
2016-01-26 14:38:28 +0100

    building menu

--- src/main/app/view/View.java
+++ src/main/app/view/View.java
@@ -12,6 +12,7 @@ import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -24,19 +25,54 @@ import wrappers.CrystallizationData;
 
 public class View extends JFrame implements ActionListener, ListDataListener {
 	private static final long serialVersionUID = 1L;
-	private DataModel model;
-	private DataListListener guiListener;
 	
+	private static final String S_FILE = "File";
+	private static final String S_NEW = "New";
+	private static final String S_OPEN = "Open";
+	private static final String S_SAVE = "Save datalist";
+	private static final String S_SAVE_AS = "Save datalist as...";
+	private static final String S_EXIT = "Exit";
+	
+	private static final String S_CALC = "Calculations";
+	private static final String S_BASIC = "Basic analysis";
+	private static final String S_AVRAMI = "Avrami analysis";
+	private static final String S_OZAWA = "Ozawa analysis";
+	private static final String S_MO = "Mo analysis";
+	private static final String S_NUCLEA = "Nucleation activity";
+	private static final String S_ENERGY = "Effective activation energy";
+	
+	private static final String S_HELP = "Help";
+	private static final String S_HELP_CONT = "Help contents";
+	private static final String S_ABOUT = "About";
+	
+	private DataModel model;
 	private JPanel dataPanel;
+	
 	private JList<CrystallizationData> dataList; 
+	private DataListListener listListener;
 	private JButton add;
 	private JButton remove;
 	private final JFileChooser addChooser = new JFileChooser();
+	
 	private JMenuBar menuBar;
 	private JMenu fileMenu;
-	private JMenu calculationMenu;
-	private JMenu helpMenu;
+	private JMenuItem itemNew;
+	private JMenuItem itemOpen;
+	private JMenuItem itemSave;
+	private JMenuItem itemSaveAs;
+	private JMenuItem itemExit;
 	
+	private JMenu calculationMenu;
+	private JMenuItem itemBasic;
+	private JMenuItem itemAvrami;
+	private JMenuItem itemOzawa;
+	private JMenuItem itemMo;
+	private JMenuItem itemNucleation;
+	private JMenuItem itemEnergy;
+	
+	private JMenu helpMenu;
+	private JMenuItem itemAbout;
+	private JMenuItem itemHelp;
 	public View(DataModel model){
 		super("Crystallization Kinetics");
 		
@@ -46,7 +82,7 @@ public class View extends JFrame implements ActionListener, ListDataListener {
 		
 		super.add(dataPanel);
 		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		super.setSize(new Dimension(600, 200));
+		super.setSize(new Dimension(500, 200));
 		super.setVisible(true);
 	}
 
@@ -54,7 +90,7 @@ public class View extends JFrame implements ActionListener, ListDataListener {
 		this.model = model;
 	}
 	public void setGuiListener(DataListListener listener){
-		guiListener = listener;
+		listListener = listener;
 	}
 	private void initMainPanel(){		
 		dataList = new JList<CrystallizationData>(model);
@@ -84,14 +120,55 @@ public class View extends JFrame implements ActionListener, ListDataListener {
 	
 	private void initMenuBar() {
 		menuBar = new JMenuBar();
-		
-		fileMenu = new JMenu("File");
-		menuBar.add(fileMenu);
-		
-		calculationMenu = new JMenu("Calculations");
+		//add file menu...
+		fileMenu = new JMenu(S_FILE);
+		itemNew = new JMenuItem(S_NEW);
+		itemNew.addActionListener(this);
+		itemSave = new JMenuItem(S_SAVE);
+		itemSave.addActionListener(this);
+		itemSaveAs = new JMenuItem(S_SAVE_AS);
+		itemSaveAs.addActionListener(this);
+		itemOpen = new JMenuItem(S_OPEN);
+		itemOpen.addActionListener(this);
+		itemExit = new JMenuItem(S_EXIT);
+		itemExit.addActionListener(this);
+		fileMenu.add(itemNew);
+		fileMenu.addSeparator();
+		fileMenu.add(itemOpen);
+		fileMenu.add(itemSave);
+		fileMenu.add(itemSaveAs);
+		fileMenu.addSeparator();
+		fileMenu.add(itemExit);
+		menuBar.add(fileMenu);		
+		//add calculation menu
+		calculationMenu = new JMenu(S_CALC);
+		itemBasic = new JMenuItem(S_BASIC);
+		itemBasic.addActionListener(this);
+		itemAvrami = new JMenuItem(S_AVRAMI);
+		itemAvrami.addActionListener(this);
+		itemOzawa = new JMenuItem(S_OZAWA);
+		itemOzawa.addActionListener(this);
+		itemMo = new JMenuItem(S_MO);
+		itemMo.addActionListener(this);
+		itemNucleation = new JMenuItem(S_NUCLEA);
+		itemNucleation.addActionListener(this);
+		itemEnergy = new JMenuItem(S_ENERGY);
+		itemEnergy.addActionListener(this);
+		calculationMenu.add(itemBasic);
+		calculationMenu.add(itemAvrami);
+		calculationMenu.add(itemOzawa);
+		calculationMenu.add(itemMo);
+		calculationMenu.add(itemNucleation);
+		calculationMenu.add(itemEnergy);
 		menuBar.add(calculationMenu);
 		
-		helpMenu = new JMenu("Help");
+		helpMenu = new JMenu(S_HELP);
+		itemHelp = new JMenuItem(S_HELP_CONT);
+		itemHelp.addActionListener(this);
+		itemAbout = new JMenuItem(S_ABOUT);
+		itemAbout.addActionListener(this);
+		helpMenu.add(itemHelp);
+		helpMenu.add(itemAbout);
 		menuBar.add(helpMenu);
 		
 		super.setJMenuBar(menuBar);
@@ -99,10 +176,10 @@ public class View extends JFrame implements ActionListener, ListDataListener {
 	
 	public void actionPerformed(ActionEvent event) {
 		if(event.getSource()==add){
-			guiListener.addPerformed();
+			listListener.addPerformed();
 		}
 		if(event.getSource()==remove){
-			guiListener.removePerformed(dataList.getSelectedIndices());
+			listListener.removePerformed(dataList.getSelectedIndices());
 			dataList.clearSelection();
 		}		
 	}
